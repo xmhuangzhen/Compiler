e64955af0df2eb5414e8602c817b6f080520946a
xmhuangzhen <xmhuangzhen@sjtu.edu.cn>
2021-01-21 16:17:23 +0800

    In the lessening gloom the valley itself took shape

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,13 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 
 public class Main {
     public static void main(String[] args) throws Exception{
 
-        String name = "System.in";
-        InputStream input = new FileInputStream(name);
+        InputStream input = System.in;
 
         try {
             RootNode ASTRoot;
