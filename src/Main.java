3d351656caf866a535ff0073e217b5cd152f89cb
xmhuangzhen <xmhuangzhen@sjtu.edu.cn>
2021-04-18 00:16:52 +0800

    and moss creeping up its foot

--- src/Main.java
+++ src/Main.java
@@ -27,9 +27,9 @@ public class Main {
 
         InputStream input = null;
        //     if(args.length != 0)
-        input = new FileInputStream("test.mx");
+     //   input = new FileInputStream("test.mx");
         //  else
- //             input = System.in;
+              input = System.in;
 
         try {
             RootNode ASTRoot;
@@ -108,7 +108,7 @@ public class Main {
                     new SSADestructor(currentModule);
             tmpSSADestructor.run();
             //--------Opt End------
-            new IRPrinter("output.ll").run(currentModule);
+        //    new IRPrinter("output.ll").run(currentModule);
 
 
 
