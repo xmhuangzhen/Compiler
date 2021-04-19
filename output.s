2fb108d0f65ea61761d960a3bbe6e3c1399a106b
xmhuangzhen <xmhuangzhen@sjtu.edu.cn>
2021-04-19 10:20:11 +0800

    entire of itself

--- output.s
+++ output.s
@@ -1,419 +1,57 @@
 	.text
-	.globl	test					# start function : test
-	.p2align	2
-test:
-#LBB40:
-	sw ra,-4(sp)
-	sw s0,-8(sp)
-	sw s1,-12(sp)
-	sw s2,-16(sp)
-	mv s0,sp
-	addi sp,sp,-120
-	mv t1,a0
-	mv a0,a1
-	mv a1,a2
-	mv a2,a3
-	mv a3,a4
-	mv t0,a6
-	mv a6,a7
-	lw a7,0(s0)
-	lw t2,4(s0)
-	xor s1,t1,a0
-	seqz s1,s1
-	bnez s1,LBB41
-	j LBB46
-LBB41:
-	xor s1,a0,a1
-	snez s2,s1
-	li s1,0
-	xor s1,s2,s1
-	seqz s1,s1
-	bnez s1,LBB42
-	j LBB43
-LBB42:
-	xor a4,a1,a2
-	snez a4,a4
-LBB43:
-	or a4,s2,a4
-	bnez a4,LBB44
-	j LBB45
-LBB44:
-	mv a4,a5
-	mv a5,t0
-	sw t2,0(sp)
-	sw t1,4(sp)
-	call test
-	addi a0,a0,1
-	j LBB47
-LBB45:
-	addi a4,a5,-1
-	addi a5,t0,-2
-	sw t2,0(sp)
-	sw t1,4(sp)
-	call test
-	addi a0,a0,2
-	j LBB47
-LBB46:
-	add a0,t1,a0
-	add a0,a0,t1
-LBB47:
-	addi sp,sp,120
-	lw s0,-8(sp)
-	lw s1,-12(sp)
-	lw s2,-16(sp)
-	lw ra,-4(sp)
-	ret
-# end function : test
 	.globl	main					# start function : main
 	.p2align	2
 main:
-#LBB48:
+#LBB40:
 	sw ra,-4(sp)
 	sw s0,-8(sp)
-	sw s1,-12(sp)
-	sw s2,-16(sp)
 	mv s0,sp
-	addi sp,sp,-120
+	addi sp,sp,-80
+LBB41:
+LBB42:
+LBB43:
+LBB44:
+	li a2,0
+	and a0,a2,a0
+	bnez a0,LBB45
+	j LBB46
+LBB45:
+	li a0,10
+	j LBB47
+LBB46:
+	li a0,20
+LBB47:
+	li a2,10
+	xor a2,a0,a2
+	seqz a3,a2
+	bnez a3,LBB48
+	j LBB49
+LBB48:
+	li a2,5
+	li a1,0
+	div a2,a2,a1
+	li a1,0
+	xor a1,a2,a1
+	seqz a1,a1
 LBB49:
+	and a1,a3,a1
+	bnez a1,LBB50
+	j LBB51
 LBB50:
 LBB51:
-	li s1,0
-	li s2,19260817
-LBB52:
-LBB53:
-LBB54:
-	mv a0,s2
-	li a1,13
-	sll a0,a0,a1
-LBB55:
-LBB56:
-	xor a2,s2,a0
-LBB57:
-	mv a1,a2
-	li a0,17
-	li a3,0
-	slt a3,a1,a3
-	xori a3,a3,1
-	bnez a3,LBB58
-	j LBB59
-LBB58:
-	sra a0,a1,a0
-	j LBB60
-LBB59:
-	li a3,31
-	sub a4,a3,a0
-	li a3,1
-	sll a4,a3,a4
-	li a3,-2147483648
-	xor a1,a1,a3
-	sra a0,a1,a0
-	or a0,a4,a0
-LBB60:
-LBB61:
-	xor a1,a2,a0
-LBB62:
-	mv a0,a1
-	li a2,5
-	sll a0,a0,a2
-LBB63:
-LBB64:
-	xor a1,a1,a0
-	li a0,1073741823
-	and a3,a1,a0
-LBB65:
-LBB66:
-LBB67:
-	mv a1,a3
-LBB68:
-	mv a0,a1
-	li a2,13
-	sll a0,a0,a2
-LBB69:
-LBB70:
-	xor a2,a1,a0
-LBB71:
-	mv a1,a2
-	li a0,17
-	li a4,0
-	slt a4,a1,a4
-	xori a4,a4,1
-	bnez a4,LBB72
-	j LBB73
-LBB72:
-	sra a0,a1,a0
-	j LBB74
-LBB73:
-	li a4,31
-	sub a5,a4,a0
-	li a4,1
-	sll a5,a4,a5
-	li a4,-2147483648
-	xor a1,a1,a4
-	sra a0,a1,a0
-	or a0,a5,a0
-LBB74:
-LBB75:
-	xor a1,a2,a0
-LBB76:
-	mv a0,a1
-	li a2,5
-	sll a0,a0,a2
-LBB77:
-LBB78:
-	xor a1,a1,a0
-	li a0,1073741823
-	and a1,a1,a0
-LBB79:
-LBB80:
-	andi a2,a3,255
-	andi a0,a1,255
-	xor a0,a2,a0
-	snez a0,a0
-	bnez a0,LBB81
-	j LBB152
-LBB81:
-LBB82:
-LBB83:
-	mv a0,a1
-	li a2,13
-	sll a0,a0,a2
-LBB84:
-LBB85:
-	xor a2,a1,a0
-LBB86:
-	mv a1,a2
-	li a0,17
-	li a3,0
-	slt a3,a1,a3
-	xori a3,a3,1
-	bnez a3,LBB87
-	j LBB88
-LBB87:
-	sra a0,a1,a0
-	j LBB89
-LBB88:
-	li a3,31
-	sub a4,a3,a0
-	li a3,1
-	sll a4,a3,a4
-	li a3,-2147483648
-	xor a1,a1,a3
-	sra a0,a1,a0
-	or a0,a4,a0
-LBB89:
-LBB90:
-	xor a1,a2,a0
-LBB91:
-	mv a0,a1
-	li a2,5
-	sll a0,a0,a2
-LBB92:
-LBB93:
-	xor a1,a1,a0
-	li a0,1073741823
-	and a4,a1,a0
-LBB94:
-LBB95:
-LBB96:
-	mv a1,a4
-LBB97:
-	mv a0,a1
-	li a2,13
-	sll a0,a0,a2
-LBB98:
-LBB99:
-	xor a2,a1,a0
-LBB100:
-	mv a1,a2
-	li a0,17
-	li a3,0
-	slt a3,a1,a3
-	xori a3,a3,1
-	bnez a3,LBB101
-	j LBB102
-LBB101:
-	sra a0,a1,a0
-	j LBB103
-LBB102:
-	li a3,31
-	sub a5,a3,a0
-	li a3,1
-	sll a5,a3,a5
-	li a3,-2147483648
-	xor a1,a1,a3
-	sra a0,a1,a0
-	or a0,a5,a0
-LBB103:
-LBB104:
-	xor a1,a2,a0
-LBB105:
-	mv a0,a1
-	li a2,5
-	sll a0,a0,a2
-LBB106:
-LBB107:
-	xor a1,a1,a0
-	li a0,1073741823
-	and a3,a1,a0
-LBB108:
-LBB109:
-LBB110:
-	mv a1,a3
-LBB111:
-	mv a0,a1
-	li a2,13
-	sll a0,a0,a2
-LBB112:
-LBB113:
-	xor a2,a1,a0
-LBB114:
-	mv a1,a2
-	li a0,17
-	li a5,0
-	slt a5,a1,a5
-	xori a5,a5,1
-	bnez a5,LBB115
-	j LBB116
-LBB115:
-	sra a0,a1,a0
-	j LBB117
-LBB116:
-	li a5,31
-	sub a6,a5,a0
-	li a5,1
-	sll a6,a5,a6
-	li a5,-2147483648
-	xor a1,a1,a5
-	sra a0,a1,a0
-	or a0,a6,a0
-LBB117:
-LBB118:
-	xor a1,a2,a0
-LBB119:
-	mv a0,a1
-	li a2,5
-	sll a0,a0,a2
-LBB120:
-LBB121:
-	xor a1,a1,a0
-	li a0,1073741823
-	and a5,a1,a0
-LBB122:
-LBB123:
-LBB124:
-	mv a1,a5
-LBB125:
-	mv a0,a1
-	li a2,13
-	sll a0,a0,a2
-LBB126:
-LBB127:
-	xor a2,a1,a0
-LBB128:
-	mv a1,a2
-	li a0,17
-	li a6,0
-	slt a6,a1,a6
-	xori a6,a6,1
-	bnez a6,LBB129
-	j LBB130
-LBB129:
-	sra a0,a1,a0
-	j LBB131
-LBB130:
-	li a6,31
-	sub a7,a6,a0
-	li a6,1
-	sll a7,a6,a7
-	li a6,-2147483648
-	xor a1,a1,a6
-	sra a0,a1,a0
-	or a0,a7,a0
-LBB131:
-LBB132:
-	xor a1,a2,a0
-LBB133:
-	mv a0,a1
-	li a2,5
-	sll a0,a0,a2
-LBB134:
-LBB135:
-	xor a1,a1,a0
-	li a0,1073741823
-	and a7,a1,a0
-LBB136:
-LBB137:
-LBB138:
-	mv a1,a7
-LBB139:
-	mv a0,a1
-	li a2,13
-	sll a0,a0,a2
-LBB140:
-LBB141:
-	xor a2,a1,a0
-LBB142:
-	mv a1,a2
-	li a0,17
-	li a6,0
-	slt a6,a1,a6
-	xori a6,a6,1
-	bnez a6,LBB143
-	j LBB144
-LBB143:
-	sra a0,a1,a0
-	j LBB145
-LBB144:
-	li a6,31
-	sub t0,a6,a0
-	li a6,1
-	sll t0,a6,t0
-	li a6,-2147483648
-	xor a1,a1,a6
-	sra a0,a1,a0
-	or a0,t0,a0
-LBB145:
-LBB146:
-	xor a1,a2,a0
-LBB147:
-	mv a0,a1
-	li a2,5
-	sll a0,a0,a2
-LBB148:
-LBB149:
-	xor a1,a1,a0
-	li a0,1073741823
-	and s2,a1,a0
-LBB150:
-LBB151:
-	andi a0,a4,3
-	srai a1,a4,28
-	andi a2,a3,1
-	srai a3,a3,29
-	srai a4,a5,25
-	andi a5,a5,31
-	srai a6,a7,15
-	li t0,32767
-	and a7,a7,t0
-	srai t1,s2,15
-	li t0,32767
-	and t0,s2,t0
-	sw t1,0(sp)
-	sw t0,4(sp)
-	call test
-	xor s1,s1,a0
-	j LBB153
-LBB152:
-	j LBB154
-LBB153:
+	andi a2,a1,1
+	li a1,1
+	xor a1,a1,a2
+	bnez a1,LBB53
 	j LBB52
-LBB154:
-LBB155:
-	mv a0,s1
-	addi sp,sp,120
+LBB52:
+	j LBB54
+LBB53:
+	li a0,30
+LBB54:
+LBB55:
+	addi sp,sp,80
 	lw s0,-8(sp)
-	lw s1,-12(sp)
-	lw s2,-16(sp)
 	lw ra,-4(sp)
 	ret
 # end function : main
--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
         InputStream input = null;
        //     if(args.length != 0)
-     //   input = new FileInputStream("test.mx");
+   //     input = new FileInputStream("test.mx");
         //  else
               input = System.in;
 
@@ -106,7 +106,7 @@ public class Main {
                 if (!modified) break;
             }
        //     System.out.println("2");
-            new IRPrinter("output.ll").run(currentModule);
+       //     new IRPrinter("output.ll").run(currentModule);
 
 
             //(n) Destruct SSA
